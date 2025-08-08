60623a0d0901b1b65a04e24b7f6c3cdf99036be5
akanimo sam okure <akan.okure@gmail.com>
2025-08-08 21:44:13 +0100

    add product service and implement product list

--- src/main/java/com/asokure/productmicroserviceapp/ProductController.java
+++ src/main/java/com/asokure/productmicroserviceapp/ProductController.java
@@ -4,14 +4,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ProductController {
-  List<ProductInfo> productList = new ArrayList<ProductInfo>();
+
+    List<ProductInfo> productList = new ArrayList<ProductInfo>();
+
     @GetMapping("/product/details/{productid}")
-    public Product getProductInfo(@PathVariable Long productid){
-// Get Name and Desc  from product service
-        // Get price from  pricing service
-        // Get Stock Availability from inventory service
-        return new Product(productid, "iphone", "iphone is damn expensive", 999, true);
+    public Product getProductDetails(@PathVariable Long productid) {
+
+        ProductInfo productInfo = getProductInfo(productid);
+
+        return new Product(productInfo.getProductID(), productInfo.getProductName(), productInfo.getProductDesc(), 999, true);
     }
+
+    private ProductInfo getProductInfo(Long productid) {
+        populateProductList();
+
+        for(ProductInfo p : productList){
+
+            if(productid.equals(p.getProductID())){
+
+                return p;
+            }
+        }
+
+        return null;
+    }
+
+    private void populateProductList() {
+        productList.add(new ProductInfo(101L, "iphone", "iphone is damn expensive"));
+        productList.add(new ProductInfo(102L, "Book", "The Book is great"));
+        productList.add(new ProductInfo(103L, "Washing Machine", "washing machine is necessary"));
+
+    }
+
 }
--- src/main/java/com/asokure/productmicroserviceapp/ProductInfo.java
+++ src/main/java/com/asokure/productmicroserviceapp/ProductInfo.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductInfo {
-
+    private Long productID;
+    private String productName;
+    private String productDesc;
 
 }
